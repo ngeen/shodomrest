fbde38cd24a188894bad6b446d993d0f54490a2a
engin <engin@192.168.1.9>
2016-11-24 22:50:27 +0200

    kontrol düzenlemesi

--- src/main/java/com/shodom/service/Downloader.java
+++ src/main/java/com/shodom/service/Downloader.java
@@ -1,7 +1,11 @@
 package com.shodom.service;
 
+import java.io.IOException;
+
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.ExecuteWatchdog;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.io.output.ByteArrayOutputStream;
@@ -13,17 +17,29 @@ import com.shodom.model.DownloadFile;
 public class Downloader {
 
 	public String download(DownloadFile downloadFile) throws Exception {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		int exitValue;
+		ExecuteWatchdog watchdog = null;
+
+		// build up the command line to using a 'java.io.File'
 		CommandLine commandLine = new CommandLine("/bin/bash");
 		commandLine.addArgument("/var/www/html/media/gifenc.sh");
 		commandLine.addArguments(downloadFile.getUrl());
 		commandLine.addArgument(downloadFile.getFileName());
-		
-		Executor exec = new DefaultExecutor();
-		exec.setExitValue(0);
-	    PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
-	    exec.setStreamHandler(streamHandler);
-	    exec.execute(commandLine);
+
+		// create the executor and consider the exitValue '0' as success
+		Executor executor = new DefaultExecutor();
+		executor.setExitValue(0);
+
+		// create a watchdog if requested
+		watchdog = new ExecuteWatchdog(10000);
+		executor.setWatchdog(watchdog);
+
+		// handle output
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
+		executor.setStreamHandler(streamHandler);
+
+		exitValue = executor.execute(commandLine);
 
 		return outputStream.toString("UTF-8");
 	}

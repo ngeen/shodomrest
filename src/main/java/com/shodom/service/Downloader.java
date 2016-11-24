8094bfb303d66d534fdfe425a53e82640f170c32
engin <engin@192.168.1.9>
2016-11-24 23:03:25 +0200

    kontrol düzenlemesi

--- src/main/java/com/shodom/service/Downloader.java
+++ src/main/java/com/shodom/service/Downloader.java
@@ -1,14 +1,7 @@
 package com.shodom.service;
 
-import java.io.IOException;
-
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteException;
-import org.apache.commons.exec.ExecuteWatchdog;
-import org.apache.commons.exec.Executor;
-import org.apache.commons.exec.PumpStreamHandler;
-import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 
 import com.shodom.model.DownloadFile;
@@ -17,30 +10,18 @@ import com.shodom.model.DownloadFile;
 public class Downloader {
 
 	public String download(DownloadFile downloadFile) throws Exception {
-		int exitValue;
-		ExecuteWatchdog watchdog = null;
+		String result = "";
+		String[] cmd = {"/bin/bash", "-c", "/var/www/html/media/gifenc.sh"};
+		ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+		processBuilder.environment().put("link", downloadFile.getUrl());
+		processBuilder.environment().put("fileName", downloadFile.getFileName());
+		processBuilder.redirectErrorStream(true);
+		final Process process = processBuilder.start();
 
-		// build up the command line to using a 'java.io.File'
-		CommandLine commandLine = new CommandLine("/bin/bash");
-		commandLine.addArgument("/var/www/html/media/gifenc.sh");
-		commandLine.addArguments(downloadFile.getUrl());
-		commandLine.addArgument(downloadFile.getFileName());
+		result = IOUtils.toString(process.getInputStream(), Charsets.toCharset("UTF-8"));
 
-		// create the executor and consider the exitValue '0' as success
-		Executor executor = new DefaultExecutor();
-		executor.setExitValue(0);
+		process.waitFor();
 
-		// create a watchdog if requested
-		watchdog = new ExecuteWatchdog(10000);
-		executor.setWatchdog(watchdog);
-
-		// handle output
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
-		executor.setStreamHandler(streamHandler);
-
-		exitValue = executor.execute(commandLine);
-
-		return outputStream.toString("UTF-8");
+		return result;
 	}
 }

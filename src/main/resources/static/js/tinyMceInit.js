0e4445e5ebdb61861c92efb5e82663a95e278508
enginoz <enginoz@teknokent7.sinerjias.local>
2016-11-29 16:50:18 +0200

    editor ile ilgili düzenlemeler yapıldı.

--- src/main/resources/static/js/tinyMceInit.js
+++ src/main/resources/static/js/tinyMceInit.js
@@ -3,7 +3,9 @@ tinymce
 			selector : 'textarea',
 			entity_encoding : "raw",
 			menubar : false,
-			forced_root_block : "",
+			//forced_root_block : "",
+			force_p_newlines : true,
+			paste_as_text: true,
 			plugins : [
 					'advlist autolink lists link image charmap print preview anchor',
 					'searchreplace visualblocks code fullscreen',
